2854c5cbdf29d03fc0b9dffb5511b283af84f6e9
Ismail Ceyhan <105164551+ceyhanismail@users.noreply.github.com>
2024-02-11 21:23:17 +0000

    Update dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -1,14 +1,23 @@
 def dijkstra(current, nodes, distances):
+    # These are all the nodes which have not been visited yet
     unvisited = {node: None for node in nodes}
+    # It will store the shortest distance from one node to another
     visited = {}
+    # It will store the predecessors of the nodes
     currentDistance = 0
     unvisited[current] = currentDistance
+    
+    # Running the loop while all the nodes have been visited
     while True:
+        # iterating through all the unvisited node
         for neighbour, distance in distances[current].items():
+            # Iterating through the connected nodes of current_node and the weight of the edges.
             if neighbour not in unvisited: continue
             newDistance = currentDistance + distance
             if unvisited[neighbour] is None or unvisited[neighbour] > newDistance:
                 unvisited[neighbour] = newDistance
+        # Till now the shortest distance between the source node and target node 
+        # has been found. Set the current node as the target node
         visited[current] = currentDistance
         del unvisited[current]
         if not unvisited: break
